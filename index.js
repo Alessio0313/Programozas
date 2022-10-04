b561b1cf45b9c188223e094e0b26c4f98df61b96
unknown <diak@fonix.kag.hu>
2022-10-04 14:00:44 +0200

    elso comit message

---
+++ index.js
@@ -0,0 +1,107 @@
+console.log(`Alessio
+
+Samu
+Döme
+Noel
+
+Marangon Alessio Oscar
+2008.04.15
+186cm`)
+
+
+console.log(22+13)
+console.log(22-13)
+console.log(22*13)
+console.log(22/13)
+console.log(22%13)
+console.log(1.5/30*100)
+
+let variable = 15
+console.log(`A kedvenc számom ${variable}`)
+
+let a = 123
+let b = 526
+let temp
+
+ temp = a
+ a = b
+ b = temp
+console.log(`${a}
+${b}`)
+
+let suly = 65
+let magassag = 186
+console.log(65/186**2)
+
+
+
+
+
+
+// //kommentek
+
+// let variable = 'a'
+// let variable2 = 12
+// let variable3 = false
+
+// let variable4 = variable2 + 32
+
+// variable2 += 32
+
+// const constVariable = 12
+
+// constVariable = 14
+
+// console.log(constVariable)
+
+//let c = 4
+//if (c < 5) {
+ //console.log(`kisebb`)
+//} else if(c < 10) {
+ //console.log(`c egyenlo 10`)  
+//} else {
+ //console.log(`hello`)
+//}    
+
+//if(c < 5 || c > 10) {
+
+//}
+//if(c < 5 && c == 3) {
+
+//}
+
+let d = 22
+let e = 13
+let f = (d+e)
+console.log(f)
+
+let g = d-e
+console.log(g)
+
+let h = d*e
+console.log(h)
+
+let i = d/e
+console.log(i)
+
+let j = d%e
+console.log(j)
+
+let óra = 1.5
+let óraszám = 30
+let k = óra/óraszám*100
+console.log(k)
+
+let aoldal = 5
+let boldal = 12
+let coldal = 6
+let A = 2*(aoldal*boldal+aoldal*coldal+boldal*coldal)
+console.log(A)
+let V = aoldal*boldal*coldal
+console.log(V)
+
+
+
+
+
+
